61dd4622ca7399dab302d813ca29c03df2e23a24
Alex Wolski <alex.theinfinite@gmail.com>
2024-09-22 13:23:26 -0700

    Changed component files from jsx to tsx

--- aot-chat-images/src/Components/Homepage.tsx
+++ aot-chat-images/src/Components/Homepage.tsx
@@ -2,8 +2,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React, {useEffect, useState} from 'react';
 import {NotificationContainer, NotificationManager} from 'react-notifications';
 import 'react-notifications/dist/react-notifications.css'
-import Dropzone from '../Components/Dropzone';
-import StyledNavbar from '../Components/StyledNavbar'
+import Dropzone from '../Components/Dropzone.tsx';
+import StyledNavbar from '../Components/StyledNavbar.tsx'
 import '../Styles/Homepage.css';
 
 function Homepage(props) {
--- aot-chat-images/src/Components/index.tsx
+++ aot-chat-images/src/Components/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../Styles/index.css';
-import Homepage from './Homepage';
+import Homepage from './Homepage.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
