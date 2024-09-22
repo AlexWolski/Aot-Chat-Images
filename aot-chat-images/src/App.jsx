4fd4ed172d6bb74e19a231811d9f3d3ecd75749f
Alex Wolski <alex.theinfinite@gmail.com>
2024-09-21 21:52:22 -0700

    Added error notificaiton

--- aot-chat-images/src/App.jsx
+++ aot-chat-images/src/App.jsx
@@ -1,12 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {useDropzone} from 'react-dropzone';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 import './App.css';
+import 'react-notifications/dist/react-notifications.css'
 
 function App(props) {
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    onDropRejected: () => {
+      NotificationManager.error('Error message');
+    },
+
+    validator: () => ({
+      code: "not-allowed",
+      message: "Failed",
+    }),
+  });
 
   return (
     <section className="container">
+      <NotificationContainer/>
+
       <div {...getRootProps({className: 'dropzone'})}>
         <input {...getInputProps()} />
         <p>Select an image file</p>
