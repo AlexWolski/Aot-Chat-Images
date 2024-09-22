d6ca0ff33dc2815f6400ffb70733e77e7e54f676
Alex Wolski <alex.theinfinite@gmail.com>
2024-09-22 13:18:16 -0700

    Ejected webpack to move index.jsx file

--- aot-chat-images/src/Components/index.jsx
+++ aot-chat-images/src/Components/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Styles/index.css';
-import Homepage from './Components/Homepage';
+import '../Styles/index.css';
+import Homepage from './Homepage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
